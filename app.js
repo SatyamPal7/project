4e0dc68b256cf0a4f81230d19f4939c48de520d1
Satyam <satyam.mj7@gmail.com>
2020-02-04 15:56:46 +0530

    last hosting

--- app.js
+++ app.js
@@ -1,60 +1,76 @@
-const inp = document.getElementById('inp')
-inp.addEventListener('click', writeData)
+const store = document.getElementById('store')
+store.addEventListener('click', st)
 
-function writeData() {
+function st() {
   console.log("Button clicked")
   //personal details
-  var first = document.getElementById("first").value;
-  var last = document.getElementById("last").value;
+  var candidate_Name = document.getElementById("name").value;
   var date = document.getElementById("date").value;
   var fathername = document.getElementById("fname").value;
   var mothername = document.getElementById("mname").value;
-  var male = document.getElementById("male").value;
-  var female = document.getElementById("female").value;
-  var other = document.getElementById("other").value;
+  var nation = document.getElementById("nation").value;
+  var category = document.getElementById("cate").value;
+  var language1 = document.getElementById("lang").value;
+  var language2 = document.getElementById("lang1").value;
+  var gender = document.getElementById("gen").value;
+  var employement = document.getElementById("employ").value;
+  var disability = document.getElementById("pwd").value;
+  var paper = document.getElementById("apply").value;
+  var offer = document.getElementById("offer").value;
+  var center = document.getElementById("center").value;
+  var medium = document.getElementById("med").value;
+  var durl = document.getElementById("doc").value;
+  var status = document.getElementById("appear").value;
+  var course = document.getElementById("course").value;
+  var university = document.getElementById("board").value;
+  var year = document.getElementById("year").value;
+  var obtain = document.getElementById("obt").value;
+  var total = document.getElementById("total").value;
+  var per = document.getElementById("percent").value;
 
-  //contact details
-  var address=document.getElementById("address").value;
-  var city = document.getElementById("city").value;
-  var pincode=document.getElementById("pincode").value;
-  var email= document.getElementById("email").value;
-  var mobile= document.getElementById("mob").value;
 
-  //password
-  var password = document.getElementById("password").value;
-  var secques= document.getElementById("ques").value;
-  var answer = document.getElementById("ans").value;
-
-  auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred)
-    return db.collection('test').doc(cred.user.uid).set({
-
-      candidate_Name: first+' '+last,
+  // auth.createUserWithEmailAndPassword(email, password).then(cred => {
+  //   console.log(cred)
+  //   return db.collection('teacher').doc(cred.user.uid).set({
+let data={
+      candidate_Name:candidate_Name,
       Date_Of_Birth: date,
-      Mother_Name:    mothername,
-      Father_Name:    fathername,
-      Gender:         male,
-      Gender:         female,
-      Gender:         other,
-      Address:        address,
-      City:           city,
-      Pincode:        pincode,
-      Email:          email,
-      Mobile:         mobile,
-      Security_Question: secques,
-      Answer:         answer,
-    });
-  }).then(ref => {
-    // close the signup modal & reset form
-    console.log("User added successfully with reference \n" + ref)
-    db.collection("test").get().then(function(querySnapshot) {
-      querySnapshot.forEach(function(doc) {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
-      });
-    });
-  }).catch(err => {
-    console.log("Error adding user\n" + err)
-  });
+      Father_Name: fathername,
+      Mother_Name: mothername,
+      Nationality: nation,
+      category:category,
+      language: language1+' ,'+language2,
+      Gender: gender,
+      employement:employement,
+      disability:disability,
+      paper:paper,
+      offer:offer,
+      center:center,
+      medium:medium,
+      durl:durl,
+      status:status,
+      course:course,
+      university:university,
+      year:year,
+      obtain:obtain,
+      total:total,
+      per:per,
+    }
 
-}
+
+
+    // close the signup modal & reset form
+
+    db.collection("teacher").add(data)
+    .then(function() {
+    console.log("Document successfully written!");
+    window.location.href="Thankyou.html";
+})
+.catch(function(error) {
+    console.error("Error writing document: ", +error);
+});
+  }
+// }  .catch(err => {
+//     console.log("Error adding user\n" + err)
+//   });
+// }
